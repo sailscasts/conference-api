feb20f43d1bedb7f3f4b6a7c65c6d2eb0470c5ff
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-16 00:14:28 +0100

    feat: define model for conference

---
+++ api/models/Conference.js
@@ -0,0 +1,57 @@
+/**
+ * Conference.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: 'string',
+      required: true,
+      minLength: 5,
+    },
+    city: {
+      type: 'string',
+    },
+    country: {
+      type: 'string',
+      required: true,
+    },
+    type: {
+      type: 'string',
+      isIn: ['online', 'in-person', 'online & in-person'],
+      defaultsTo: 'in-person',
+    },
+    location: {
+      type: 'string',
+      required: true,
+    },
+    cfpUrl: {
+      columnName: 'cfp_url',
+      type: 'string',
+      isURL: true,
+      allowNull: true,
+    },
+    twitterUrl: {
+      columnName: 'twitter_url',
+      type: 'string',
+      isURL: true,
+      allowNull: true,
+    },
+    website: {
+      type: 'string',
+      isURL: true,
+    },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+}
