df11ace6361549074746f5b80fb48f537bfb395b
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-01-20 13:15:13 +0100

    feat: add baseUrl for local

--- config/custom.js
+++ config/custom.js
@@ -16,5 +16,6 @@ module.exports.custom = {
    ***************************************************************************/
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
+  baseUrl: 'http://localhost:1337',
   // …
 }
